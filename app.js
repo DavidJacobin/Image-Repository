434038b640b8e617ee727b0f031b7d491a1cdf2a
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-01-04 10:14:31 +0200

    added upload folder and upload code

--- app.js
+++ app.js
@@ -5,11 +5,32 @@ const fileUpload = require("express-fileupload");
 const app = express();
 
 app.use(fileUpload());
-app.engine("hbs", exphbs.engine({ extname: '.hbs'}));
+app.engine("hbs", exphbs.engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.post('/', (req, res) => {
+
+    let sampleFile;
+    let uploadPath;
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded')
+    };
+
+    sampleFile = req.files.sampleFile;
+    uploadPath = __dirname + '/upload/' + sampleFile.name;
+
+    sampleFile.mv(uploadPath, (err) => {
+        if (err) return res.status(500).send(err);
+
+        res.send('File uploaded!')
+    })
+
+
 });
 
 
